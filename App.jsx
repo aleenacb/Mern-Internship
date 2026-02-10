de4930ad50c5dd1f995d9e3ca9cc6db9c7001d0e
aleenacb <aleenacbenoy@gmail.com>
2026-02-10 12:33:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Firstapp from './Components/Firstapp'
+function App() {
+  return (
+    <div>
+      <h1 style={{color:"blue"}}>Inline CSS</h1>
+      <Firstapp/>
+    </div>
+  )
+};
+export default App
