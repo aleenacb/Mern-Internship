2716cb4fa93db70b46f6ed35eb933c5724d67ef4
aleenacb <aleenacbenoy@gmail.com>
2026-02-11 22:02:57 +0530

    Update ArrowFunctions.jsx

--- ArrowFunctions.jsx
+++ ArrowFunctions.jsx
@@ -1,44 +1,49 @@
 import Button from '@mui/material/Button'
 import React from 'react'
 
+// 1. Normal Function Component
+function MyFunction() {
+    return <h3>Normal Function</h3>
+}
+
+// 2. Arrow Function Component
+const ArrowFunction = () => {
+    return (
+        <div>
+            <h3>Arrow function using multiple statements</h3>
+            <h5>Hello</h5>
+        </div>
+    )
+}
+
+// 3. Single Line Arrow Component
+const SingleArrow = () => <h3>Single Arrow Function</h3>
+
 export default function ArrowFunctions() {
-    // 1. Normal function
 
-    function Myfunction() {
-        return (
-            <h3>Normal function</h3>
-            
-        )
+    const HandleClick = () => alert("Button Clicked!")
+
+    const HandleWelcome = (name) => {
+        alert("Hey! " + name)
     }
-    // 2. Arrow function
-const Arrow = () => {
-    return(
-        <>
-            <div>
-                <h3>Arrow function using multiple statements</h3>
-                <h5>Hello</h5>
-            </div>
-        </>
-    )
-}
 
-// 3. Arrow function with single statement
-const Singlearrow = () => <h3>Arrow function with single statement</h3>
-const HandleClick = () => alert("Button Clicked!")
-const Handlewelcome = (name) => {
-    return(
-        alert("Hey!" +name)
+    return (
+        <div>
+            <MyFunction />
+            <ArrowFunction />
+            <SingleArrow />
+
+            <Button variant="contained" onClick={HandleClick} color="success">
+                Click Me
+            </Button>
+
+            <Button variant="contained" onClick={() => console.log("Hello!")} color="success">
+                Welcome
+            </Button>
+
+            <Button onClick={() => HandleWelcome("Aleena")}>
+                Submit
+            </Button>
+        </div>
     )
-}
-  return (
-    <div>
-      <Myfunction/>
-      <Arrow/>
-      <Singlearrow/>
-      
-      <Button variant="contained" onClick={HandleClick}color="success">Click Me</Button>
-      <Button variant="filled" onClick={()=>console.log("Hello!")} color='success'>Welcome</Button>
-      <Button onClick={()=>Handlewelcome("Aleena")}>Submit</Button>
-    </div>
-  )
 }
