c90ce013240429e3126fb1dad731e1e849392659
aleenacb <aleenacbenoy@gmail.com>
2026-02-18 14:02:44 +0530

    Add files via upload

---
+++ myapp/Components/MUI/Register.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import MUIAppbar from './MUIAppbar';
+export default function Register() {
+  return (
+    <div>
+      <MUIAppbar/>
+      <Paper elevation={20} style={{width:"500px", padding:"30px"}}>
+      <Typography variant='h4'>Register Page</Typography>
+      <TextField variant='filled' type='text'label="Name" fullWidth style={{marginBottom:"20px", marginLeft:"5px"}}></TextField>
+      <TextField variant='filled' type='email' label="Email" fullWidth style={{marginBottom:"20px"}}/>
+      <TextField variant='outlined' type='password' label="Password" fullWidth style={{marginBottom:"20px"}}/>
+      <TextField variant='standard'type='phone' label="Phone" fullWidth style={{marginBottom:"20px"}}/>
+      <TextField variant='filled'multiline rows = {5}type='address' label="Address" fullWidth style={{marginBottom:"20px"}}/>
+      <Button variant='contained' fullWidth>Register</Button>
+     </Paper>
+    </div>
+  )//task->Go through mui (Components)
+}
