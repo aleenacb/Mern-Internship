6e1266d131ffcfa74bc00f816caac1e7c0ad9d5b
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-09 23:51:59 +0530

    update

--- dev.to-clone/client/public/src/components/server/controllers/user_controller.js
+++ dev.to-clone/client/public/src/components/server/controllers/user_controller.js
@@ -1,13 +1,13 @@
-const usertable = require("../Models/User_model")
+const usertable = require("../models/user_models")
 
 const registeruser = async(req, res)=> {
     try{
-        const{uname, uemail, upassword, uphone} = req.body
+        const{name, email, password, phone} = req.body
         const userdetails = new usertable({
-            name: uname,
-            email:uemail,
-            password:upassword,
-            phone:uphone
+            name,
+            email,
+            password,
+            phone
         })
         await userdetails.save();
         res.status(201).json(
@@ -18,4 +18,55 @@ const registeruser = async(req, res)=> {
         res.status(500).json({message:"server error", error})
     }
 }
-module.exports = registeruser
+const getUser = async(req, res)=> {
+    try {
+        const getallUsers = await usertable.find()
+        console.log(getallUsers)
+        res.status((200).json({message:"User fetched", allusers:getallUsers}))
+    } catch (error) {
+        console.log(error)
+        res.status((500).json({message:"server error",error}))
+    }
+}
+
+const getuserbyid=async(req, res)=> {
+    try {
+        const uid = req.params.id
+        const userbyid = await usertable.findById
+        console.log(userbyid)
+        res.status(200).json({message:"user found",byid:userbyid})
+    } catch (error) {
+        console.log(error)
+        res.status((500).json({message:"server error",error}))
+    }
+}
+const deleteuser = async(req, res)=>{
+    try {
+        const d_id = req.params.id
+        const deleteuser = await usertable.findByIdAndDelete(d_id)
+        console.log(deleteuser)
+        res.status(200).json({message:"user deleted", d_user:deleteuser})
+    } catch (error) {
+    console.log(error)
+    res.status((500).json({message:"server error",error}))
+    }
+}
+
+const updateuser = async(req,res)=>{
+    try {
+    //    const u_id = req.params.id 
+        const {id} = req.params
+        const body = req.body
+        const updateuser = await usertable.findByIdAndUpdate(id, body,{new:true})
+        console.log(updateuser)
+        res.status(201).json({message:"user updated", updatedata:updateuser})
+    } catch (error) {
+        console.log(error)
+        res.status((500).json({message:"server error", error}))
+    }
+}
+// module.exports = registeruser
+// module.exports = getUser
+
+// or 
+module.exports = {registeruser, getUser, getuserbyid, deleteuser, updateuser}
