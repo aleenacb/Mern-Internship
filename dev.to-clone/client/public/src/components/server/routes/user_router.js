67e4297906bad6fe9de4010a09fb1f7fd1e927b8
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-09 23:52:45 +0530

    Update user_router.js

--- dev.to-clone/client/public/src/components/server/routes/user_router.js
+++ dev.to-clone/client/public/src/components/server/routes/user_router.js
@@ -1,7 +1,24 @@
 const express = require('express');
-const registeruser = require('../controllers/user_controller');
+const {registeruser, getUser, getuserbyid, deleteuser, updateuser} = require('../controllers/user_controller');
 
 const route = express.Router();
 
 route.post('/registeruser',registeruser)
-registeruser = require('../controllers/user_controller')
+// registeruser = require('../controllers/user_controller')
+route.get('/getUser', getUser)
+route.get('/getuserbyid/:id',getuserbyid)
+route.delete('/deleteuser/:id', deleteuser)
+route.put('/updateuser/:id', updateuser)
+module.exports = route
+
+/*const express = require("express");
+
+const { registerUser, getUsers } = require("../controllers/userController");
+
+const router = express.Router();
+
+router.post("/register", registeruser);
+
+router.get("/users", getUsers);
+
+module.exports = router;*/
