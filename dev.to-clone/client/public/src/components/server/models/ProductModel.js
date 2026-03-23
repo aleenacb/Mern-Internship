6f24f847ecf52a09b35a8ccfef286b3897ed2083
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-23 20:32:33 +0530

    Revise ProductModel schema with new fields

--- dev.to-clone/client/public/src/components/server/models/ProductModel.js
+++ dev.to-clone/client/public/src/components/server/models/ProductModel.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    //quantity: { type: Number },
-    category: { type: String, required: true }
-})
+    description: { type: String },
+    price: { type: Number },
+    quantity: { type: Number },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'category'
+    },
+    productimage:{type:String}
+    
+}, {
+    timestamps: true  
+});
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model('product', productSchema);
