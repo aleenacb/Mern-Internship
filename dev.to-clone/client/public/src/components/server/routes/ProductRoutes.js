6f9ed458fe516d3c90fecccadaaf83e4eb5a4d93
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-25 22:51:47 +0530

    update

--- dev.to-clone/client/public/src/components/server/routes/ProductRoutes.js
+++ dev.to-clone/client/public/src/components/server/routes/ProductRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { addProduct, getProducts, editProduct, deleteProduct  } = require('../Controller/ProductController');
-
+const { addProduct, getProducts, UpdateProduct, deleteProduct } = require('../controllers/ProductController');
+const upload = require('../middleware/imageUpload')
 const route = express.Router()
 
-route.post('/addproduct', addProduct)
-route.get('/getproducts', getProducts)
-route.put('/editproduct/:id', editProduct)
-route.delete('/deleteproduct/:id', deleteProduct)
-
+route.post('/addProduct', upload.single('productimage'), addProduct)
+route.get('/getProducts', getProducts)
+route.put('/UpdateProduct/:id', upload.single('productimage'), UpdateProduct)  // ✅ added upload middleware
+route.delete('/deleteProduct/:id', deleteProduct)
 
 module.exports = route
